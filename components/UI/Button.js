6a90034dedbf6dc8b60e13c69902447e58e3e96b
egsir1 <egsir111@gmail.com>
2023-12-27 18:13:04 +0900

    feat: Expense Tracker app completed

---
+++ components/UI/Button.js
@@ -0,0 +1,45 @@
+import { Pressable, Text, View, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+
+function Button({ children, onPress, mode }) {
+  return (
+    <View>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => pressed && styles.pressed}
+      >
+        <View style={[styles.button, mode === "flat" && styles.flat]}>
+          <Text style={[styles.buttonText, mode === "flat" && styles.flatTest]}>
+            {children}
+          </Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 4,
+    paddingHorizontal: 22,
+    paddingVertical: 12,
+    backgroundColor: GlobalStyles.colors.primary500,
+  },
+  flat: {
+    backgroundColor: "transparent",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  flatText: {
+    color: GlobalStyles.colors.primary200,
+  },
+  pressed: {
+    opacity: 0.75,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+  },
+});
---
+++ screens/AllExpenses.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { useContext } from "react";
+import { ExpensesContext } from "../store/expenses-context";
+
+function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod={"Total"}
+      fallbackText={"No registerd expenses found!"}
+    />
+  );
+}
+
+export default AllExpenses;
